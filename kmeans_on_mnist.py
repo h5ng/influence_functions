65a7087dff6f35ac8c1e26ea8df7ae188edec04b
GILIM <gilim.h5ng@gmail.com>
2020-06-05 15:32:44 +0900

    kmeans with influnes

--- kmeans_on_mnist.py
+++ kmeans_on_mnist.py
@@ -7,6 +7,13 @@ from scipy.misc import comb
 
 from tensorflow.examples.tutorials.mnist import input_data
 
+def get_influence_values(list, count=10, type='abs'):
+  if type == 'abs':
+    list = [(i[0], np.absolute(i[1])) for i in list]
+
+  list = sorted(list, key=lambda x: x[1])
+  return list[:count], list[-count:]
+
 def rand_index_score(clusters, classes):
     tp_plus_fp = comb(np.bincount(clusters), 2).sum()
     tp_plus_fn = comb(np.bincount(classes), 2).sum()
@@ -33,15 +40,18 @@ def show_images(images, cols = 1, titles = None):
     """
     assert((titles is None)or (len(images) == len(titles)))
     n_images = len(images)
+    
     if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
     fig = plt.figure()
     for n, (image, title) in enumerate(zip(images, titles)):
         a = fig.add_subplot(cols, np.ceil(n_images/float(cols)), n + 1)
         if image.ndim == 2:
             plt.gray()
-        
+
+        a.axes.get_xaxis().set_visible(False)
+        a.axes.get_yaxis().set_visible(False)
         plt.imshow(image)
-        a.set_title(title)
+        # a.set_title(title)
     fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
     plt.show()
 
@@ -57,23 +67,10 @@ for i in range(len(y_train)):
   x.append(x_train[i])
   y.append(y_train[i])
 
-x = x[:1000]
-
-# s = 20
-# e = s + 20
-# show_images([i.reshape(28, 28) for i in x[s:e]], cols=4, titles=[i for i in range(s,e)])
-
-# # Show filtered images
-# # plot_images = [x[i].reshape(28, 28) for i in range(2)]
-# # show_images(plot_images, cols=5, titles=y[:2])
-
+x, y = x[:1000], y[:1000]
 
 # centroids
-# x, centroids = np.array(x), np.array([x[0], x[50]]) # 8846, 0.618782, 0.74
-# x, centroids = np.array(x), np.array([x[1], x[112]]) # 8846, 0.618782, 0.74
 x, centroids = np.array(x), np.array([x[1], x[34]]) # 8846, 0.618782, 0.74
-
-# show_images([centroids[i].reshape(28, 28) for i in range(len(centroids))])
 
 x_v = tf.constant(x, dtype=tf.float32)
 c_v = tf.Variable(centroids, dtype=tf.float32)
@@ -112,22 +109,6 @@ for i in range(100):
     feed_dict={x_p: np.expand_dims(x, axis=0),
                c_p: np.expand_dims(centroids, axis=1)})
 
-# show_images([centroid_points[i].reshape(28, 28) for i in range(len(centroid_points))])
-
-# for i in range(len(assignment_values)):
-#   if assignment_values[i] == 0:
-#     assignment_values[i] = 7
-#   else:
-#     assignment_values[i] = 1
-  
-# RI = rand_index_score(y, assignment_values)
-# print(RI)
-
-
-# score = 0
-# for i in range(len(assignment_values)):
-#   if assignment_values[i] == y[i]:
-#     score += 1
 
 PARAM = 1
 grad_op = tf.gradients(distances, [c_p])
@@ -146,7 +127,6 @@ inv_hess = np.linalg.inv(z_hess)
 
 infl = []
 for i in range(len(x0)):
-# for i in range(len(x)):
   z_grad = sess.run(
     grad_op[0][0],
     feed_dict={
@@ -154,27 +134,17 @@ for i in range(len(x0)):
       c_p: np.expand_dims([centroid_points[PARAM]], axis=1)
     }
   )
+  z_grad_hess = np.dot(z_grad, inv_hess)
+  z_grad_hess_z_grad = np.dot(z_grad_hess, np.transpose(z_grad))
   infl.append(
-    (i, -np.linalg.norm(np.dot(inv_hess, np.transpose(z_grad))))
+    (i, -z_grad_hess_z_grad[0][0])
   )
 
-infl = sorted(infl, key=lambda x: x[1])
+PLOT_CNT = 10
+_, infl = get_influence_values(infl, PLOT_CNT)
 
-harmful = infl[:5]
-helpful = infl[-5:]
+plot_images = []
+plot_images.append(centroid_points[PARAM].reshape(28, 28))
+plot_images = plot_images + [x0[infl[i][0]].reshape(28, 28) for i in range(PLOT_CNT)]
 
-
-labels = []
-harmful_images = []
-harmful_images.append(centroid_points[PARAM].reshape(28, 28))
-harmful_images = harmful_images + [x[harmful[i][0]].reshape(28, 28) for i in range(len(harmful))]
-label1 = ['harmful' for i in range(6)]
-
-helpful_images = []
-helpful_images.append(centroid_points[PARAM].reshape(28, 28))
-helpful_images = helpful_images + [x[helpful[i][0]].reshape(28, 28) for i in range(len(helpful))]
-label2 = ['helpful' for i in range(6)]
-
-
-plot_images = harmful_images + helpful_images
-show_images(plot_images, cols=2, titles=label1 + label2)
+show_images(plot_images, cols=1)
